efc02b5ac8c256853e6c3c3b76b1b8edb34450a7
Kritin Vij <kritinvij@gmail.com>
2020-11-08 00:08:50 +0000

    Update twitch-ad-blocker.js

--- twitch-ad-blocker.js
+++ twitch-ad-blocker.js
@@ -16,20 +16,40 @@
 // })();
 
 /// twitch-videoad.js
+// const origFetch = window.fetch;
+// window.fetch = (url, init, ...args) => {
+// 	if (typeof url === "string") {
+// 		if (url.includes("/access_token")) {
+// 			url = url.replace("player_type=site", "player_type=facebook");
+// 		} else if (
+// 			url.includes("/gql") &&
+// 			init &&
+// 			typeof init.body === "string" &&
+// 			init.body.includes("PlaybackAccessToken")
+// 		) {
+// 			const newBody = JSON.parse(init.body);
+// 			newBody.variables.playerType = "facebook";
+// 			init.body = JSON.stringify(newBody);
+// 		}
+// 	}
+// 	return origFetch(url, init, ...args);
+// };
+
+/// twitch-videoad.js
 const origFetch = window.fetch;
 window.fetch = (url, init, ...args) => {
 	if (typeof url === "string") {
 		if (url.includes("/access_token")) {
-			url = url.replace("player_type=site", "player_type=facebook");
+			// url = url.replace("player_type=site", "player_type=site");
 		} else if (
 			url.includes("/gql") &&
 			init &&
 			typeof init.body === "string" &&
 			init.body.includes("PlaybackAccessToken")
 		) {
-			const newBody = JSON.parse(init.body);
-			newBody.variables.playerType = "facebook";
-			init.body = JSON.stringify(newBody);
+			// const newBody = JSON.parse(init.body);
+			// newBody.variables.playerType = "site";
+			// init.body = JSON.stringify(newBody);
 		}
 	}
 	return origFetch(url, init, ...args);
