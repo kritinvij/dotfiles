74b05737f93c1939d28d09c2f632bf68626e3a17
Kritin Vij <kritinvij@gmail.com>
2023-12-21 15:18:23 +0530

    Update pullall.sh

--- pullall.sh
+++ pullall.sh
@@ -1,25 +1,28 @@
 #!/bin/bash
 
-CUR_DIR=$(pwd)
-printf "Pulling in latest changes for all repositories...\n\n"
+BASE_DIR=$(pwd);
+printf "Pulling in latest changes for all repositories...\n\n";
 
 # Find all git repositories and update it to the main latest revision
 for i in $(find . -name ".git" | cut -c 3-); do
-    printf "%s\n" $i
-
     # We have to go to the .git parent directory to call the pull command
     cd "$i";
     cd ..;
+
+    CUR_FILE_FULLPATH=$(pwd)
+    CUR_FILENAME="$(basename -- "$CUR_FILE_FULLPATH")"
+
+    printf "%s\n" $CUR_FILENAME;
 
     # Switch to main branch
     # git checkout main;
 
     # Pull
     git pull;
-    printf "\n"
+    printf "\n";
 
     # Go back to the base parent directory
-    cd $CUR_DIR
+    cd $BASE_DIR;
 done
 
-echo "Completed fetching latest changes!"
+printf "Completed fetching latest changes!";
